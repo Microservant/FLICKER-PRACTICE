0149b58cb785fe76c91c06268206d8df4d92d220
Microservant <93242642+Microservant@users.noreply.github.com>
2020-09-24 18:42:37 +0000

    another try

--- gist.el
+++ gist.el
@@ -23,3 +23,4 @@
 ;;
 ;; This is distributed in the hope that it will be useful, but WITHOUT
 ;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
