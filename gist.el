326662b07d80b0ffc2169a8d7dc40dd381adfa6a
Microservant <93242642+Microservant@users.noreply.github.com>
2020-09-25 08:50:21 +0000

    fix template name

--- gist.el
+++ gist.el
@@ -27,3 +27,5 @@
 ;; for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
