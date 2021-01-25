6399b9cd2f7553f5afaeb317757d31b5a446f560
Microservant <93242642+Microservant@users.noreply.github.com>
2021-01-25 15:51:03 +0000

    update log message

--- gist.el
+++ gist.el
@@ -141,3 +141,4 @@ created gist."
                                         (xml-mode . "xml"))
   "Mapping between major-modes and file extensions.
 Used to generate filenames for created gists, and to select
+appropriate modes from fetched gist files (based on filenames)."
