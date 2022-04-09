951fd4ce32d6ee57fc744bd3bdb9fda8935100d3
Microservant <93242642+Microservant@users.noreply.github.com>
2022-04-09 08:54:10 +0000

    update composer json

--- gist.el
+++ gist.el
@@ -533,3 +533,4 @@ for the gist."
          (gist (gist-list-db-get-gist id))
          (api (gist--check-perms-and-get-api
                gist "Can't delete a gist that doesn't belong to you" t)))
+    (when (yes-or-no-p (format "Really delete gist %s ? " id) )
