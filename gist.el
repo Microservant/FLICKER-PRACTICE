bc3001ba64a41d7aa20aea44f509ee38c8238769
Microservant <93242642+Microservant@users.noreply.github.com>
2022-07-15 11:38:40 +0000

    1.1.13

--- gist.el
+++ gist.el
@@ -629,3 +629,4 @@ put it into `kill-ring'."
         (apply 'vector
                (loop for (sym label width sort format) in gist-list-format
                      collect (list label width sort)))
+        tabulated-list-padding 2
